d1b8d82c664fa189ce2e85c38abd45474daafc99
Home <owiddison1@gmail.com>
2021-01-15 12:14:57 +0000

    Iteration 1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let running = true;
+let deck[][] = [["1","2","3","4","5","6","7","8","9","10"],["1","2","3","4","5","6","7","8","9","10"],["1","2","3","4","5","6","7","8","9","10"]];
+let shuffled[] = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
+
+while(running == true) {
+  console.log("What would you like to do:");
+  console.log("1. Play the game")
+  let choice = prompt("Please choose an option");
+  switch (choice) {
+    case "1":
+      for(let n = 0 ; n <=29 ; n++) {
+
+      }
+  }
+}
